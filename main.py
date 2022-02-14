7495215cd203208ec416dd07f3fbd5952a55666a
yuri.silenok <yuri.silenok@gmail.com>
2022-02-14 22:53:16 +0300

    Я задолбался фиксить этот баг

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import os
 
+from kivy import platform
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
+from kivyauth.google_auth import initialize_google
 
 from screens.welcome import Welcome
 from screens.clients import Clients
 
+# GOOGLE_CLIENT_ID = '.apps.googleusercontent.com'
+
+GOOGLE_CLIENT_ID = '.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = ''
 
 class MembershipAccounting(App):
 
     def build(self):
+        if platform == 'android':
+            initialize_google(self.after_login, self.error_listener)
+        else:
+            initialize_google(
+                self.after_login,
+                self.error_listener,
+                GOOGLE_CLIENT_ID,
+                GOOGLE_CLIENT_SECRET,
+            )
         sm = ScreenManager()
 
         Builder.load_file(os.path.join(os.getcwd(), 'designs', 'welcome.kv'))
