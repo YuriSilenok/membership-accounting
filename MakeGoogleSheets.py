f752aa2316e56541fec8a02b0852720f7dc67643
denischagin <denischaginnn@gmail.com>
2022-02-19 10:03:24 +0300

    18-19.02 result

--- MakeGoogleSheets.py
+++ MakeGoogleSheets.py
@@ -1,81 +1,97 @@
-from pprint import pprint
+from pprint import pp, pprint
 import httplib2
 import apiclient.discovery
+from googleapiclient.http import MediaFileUpload
 from oauth2client.service_account import ServiceAccountCredentials
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 CREDENTIALS_FILE = 'fileapi.json'  # Имя файла с закрытым ключом, вы должны подставить свое
 
 # Читаем ключи из файла
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
-                                                               ['https://www.googleapis.com/auth/spreadsheets',
+                                                               [
                                                                 'https://www.googleapis.com/auth/drive'])
 
 httpAuth = credentials.authorize(httplib2.Http())  # Авторизуемся в системе
 service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)  # Выбираем работу с таблицами и 4 версию API
 
-spreadsheet = service.spreadsheets().create(body={
-    'properties': {'title': 'Google sheets', 'locale': 'ru_RU'},
-    'sheets': [{'properties': {'sheetType': 'GRID',
-                               'sheetId': 0,
-                               'title': 'Клиенты',
-                               'gridProperties': {'rowCount': 100, 'columnCount': 15}}},
-               {'properties': {'sheetType': 'GRID',
-                               'sheetId': 1,
-                               'title': 'Абонементы',
-                               'gridProperties': {'rowCount': 100, 'columnCount': 15}}},
-               {'properties': {'sheetType': 'GRID',
-                               'sheetId': 2,
-                               'title': 'Тренировки',
-                               'gridProperties': {'rowCount': 100, 'columnCount': 15}}}
-               ]
-}).execute()
-spreadsheetId = spreadsheet['spreadsheetId']  # сохраняем идентификатор файла
-emaill = 'denischaginnn@gmail.com'
 
-driveService = apiclient.discovery.build('drive', 'v3', http=httpAuth)  # Выбираем работу с Google Drive и 3 версию API
-access = driveService.permissions().create(
-    fileId=spreadsheetId,
-    body={'type': 'user', 'role': 'writer', 'emailAddress': f'{emaill}'},  # Открываем доступ на редактирование
-    fields='id'
-).execute()
+def created():
+    spreadsheet = service.spreadsheets().create(body={
+        'properties': {'title': 'Google sheets', 'locale': 'ru_RU'},
+        'sheets': [{'properties': {'sheetType': 'GRID',
+                                   'sheetId': 0,
+                                   'title': 'Клиенты',
+                                   'gridProperties': {'rowCount': 100, 'columnCount': 15}}},
+                   {'properties': {'sheetType': 'GRID',
+                                   'sheetId': 1,
+                                   'title': 'Абонементы',
+                                   'gridProperties': {'rowCount': 100, 'columnCount': 15}}},
+                   {'properties': {'sheetType': 'GRID',
+                                   'sheetId': 2,
+                                   'title': 'Тренировки',
+                                   'gridProperties': {'rowCount': 100, 'columnCount': 15}}}
+                   ]
+    }).execute()
+    print('https://docs.google.com/spreadsheets/d/' + spreadsheet['spreadsheetId'])
+    spreadsheetId = spreadsheet['spreadsheetId']  # сохраняем идентификатор файла
+    print(f'Sheet id = {spreadsheetId}')
+    return spreadsheetId
 
-results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body={
-    "valueInputOption": "USER_ENTERED",
-    # Данные воспринимаются, как вводимые пользователем (считается значение формул)
 
-    "data": [
-        {"range": "Клиенты!A1:H1",
-         "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
-         "values": [
-             ['Идентификатор клиента',
-              "Дата регистрации",
-              "Фамилия",
-              "Имя",
-              "Отчество",
-              "Номер телефона",
-              "Количество абонементов",
-              "Дата покупки последнего абонемента"],  # Заполняем 1ую строку
-         ]},
-        {"range": "Абонементы!A1:G1",
-         "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
-         "values": [
-             ["Идентификатор абонемента",
-              "Идентификатор клиента",
-              "Дата создания",
-              "Цена абонемента",
-              "Количесво занятий",
-              "Пройденных занятий",
-              "Дата и время последней тренировки"],  # Заполняем 1ую строку
-         ]},
-        {"range": "Тренировки!A1:C1",
-         "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
-         "values": [
-             ["Идентификатор тренировки",
-              "Идентификатор абонемента",
-              "Дата и время тренировки"],  # Заполняем 1ую строку
-         ]},
-    ],
-}).execute()
+def dostup(spreadsheetId):
+    emaill = 'denischaginnn@gmail.com'
+
+    driveService = apiclient.discovery.build('drive', 'v3',
+                                             http=httpAuth)  # Выбираем работу с Google Drive и 3 версию API
+    driveService.permissions().create(
+        fileId=spreadsheetId,
+        body={'type': 'user', 'role': 'writer', 'emailAddress': f'{emaill}'},  # Открываем доступ на редактирование
+        fields='id'
+    ).execute()
+
+
+def batchupdated(spreadsheetId):
+    results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body={
+        "valueInputOption": "USER_ENTERED",
+        # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+
+        "data": [
+            {"range": "Клиенты!A1:H1",
+             "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+             "values": [
+                 ['Идентификатор клиента',
+                  "Дата регистрации",
+                  "Фамилия",
+                  "Имя",
+                  "Отчество",
+                  "Номер телефона",
+                  "Количество абонементов",
+                  "Дата покупки последнего абонемента"],  # Заполняем 1ую строку
+             ]},
+            {"range": "Абонементы!A1:G1",
+             "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+             "values": [
+                 ["Идентификатор абонемента",
+                  "Идентификатор клиента",
+                  "Дата создания",
+                  "Цена абонемента",
+                  "Количесво занятий",
+                  "Пройденных занятий",
+                  "Дата и время последней тренировки"],  # Заполняем 1ую строку
+             ]},
+            {"range": "Тренировки!A1:C1",
+             "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+             "values": [
+                 ["Идентификатор тренировки",
+                  "Идентификатор абонемента",
+                  "Дата и время тренировки"],  # Заполняем 1ую строку
+             ]},
+        ],
+    }).execute()
+
+
 # results = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={
 #     "requests": [
 #
@@ -90,4 +106,27 @@ results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetI
 #         },
 #     ]
 # }).execute()
-print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
+def geting(spreadsheetId):
+    values = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId,
+        range='A1:E10',
+        majorDimension='ROWS'
+    ).execute()
+    pprint(values)
+
+def login_and_create_folder():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()  # client_secrets.json need to be in the same directory as the script
+    drive = GoogleDrive(gauth)
+
+    folder_metadata = {
+        'title': 'New folder',
+        # The mimetype defines this new file as a folder, so don't change this.
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+    return folder['id']
+
+if __name__ == '__main__':
+    login_and_create_folder()
